3a2f731c2ff8b2a0bf0ed38595df75832ca2bd0c
lopatop <pavellopato327@gmail.com>
2024-11-20 00:02:58 +0100

    function addNote addColor addFavorite

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const MOCK_NOTES = [
     {
         id: 2,
         title: 'Работа с формами',
-        content: 'К делённым полям формы можно обратиться через form.elements по значению, указанному в атрибуте nameобратиться через form.elements по значению, указанному в атрибуте name',
+        content: 'К делённым полям формы можно обрат',
         color: 'F3DB7D',
         isFavorite: false,
 
@@ -33,10 +33,41 @@ const MOCK_NOTES = [
     },
 ]
 
+
+const colors = {
+    YELLOW: 'F3DB7D',
+    GREEN: 'C2F37D',
+    BLUE: '7DE1F3',
+    RED: 'F37D7D',
+    PURPLE: 'E77DF3',
+}
+
+
 const model = {
-    notes: MOCK_NOTES,
-    addNode(id, title, content, color, isFavorite = true) {
-        const note = null;
+    notes: [],
+    noteColor: 'F3DB7D',
+
+    addNote(title, content) {
+        const note = { id: new Date().getTime(), title: title, content, color: this.noteColor, isFavorite: false }
+        this.notes.unshift(note)
+        viev.renderNotes(this.notes)
+        viev.renderNotesCount()
+    },
+    addColor(color) {
+        this.noteColor = colors[color.toUpperCase()]
+    },
+    addFavorite(noteId) {
+        this.notes.forEach((note) => {
+            if (note.id === noteId) {
+                note.isFavorite = !note.isFavorite
+            }
+        })
+        viev.renderNotes(this.notes)
+    },
+
+
+    renderNotesCount() {
+        return this.notes.length
     },
 
 }
@@ -46,41 +77,89 @@ const viev = {
     init() {
         this.renderNotes(model.notes)
         this.renderNotesCount()
+
+        const form = document.querySelector('.form');
+        const textTitle = document.querySelector('.text-title');
+        const textContent = document.querySelector('.text-content');
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            const title = textTitle.value;
+            const content = textContent.value;
+            controller.addNote(title, content);
+            textTitle.value = '';
+            textContent.value = '';
+        })
+
+        const radioColors = document.querySelectorAll('.radio');
+        radioColors.forEach((radio) => {
+            radio.addEventListener('click', (event) => {
+                const color = event.target.value;
+                controller.addColor(color);
+
+            });
+        });
+
+        const list = document.querySelector('.notes-list');
+        list.addEventListener('click', (event) => {
+            if (event.target.closest('.favorite-btn')) {
+                const noteId = +event.target.closest('li').id;
+                controller.addFavorite(noteId);
+            }
+        })
+
     },
-    renderNotesCount(){
-        const countNotes = document.querySelector('.count_notes');
-        countNotes.textContent = model.notes.length;
-    },
-    getShowFavorites(){
-    const filterBox = document.querySelector('#show-favorites')
-    filterBox.addEventListener(click,(event) =>{
-        if(event.target.classList.contains(''))
-    })
-    },
+
 
     renderNotes(notes) {
         const list = document.querySelector('.notes-list');
         let notesHTML = '';
-        for (let i = 0; i < notes.length; i++) {
-        const note = notes[i]
+        notes.forEach((note) => {
             notesHTML += ` 
             <li id="${note.id}">
     <div class="notes-title" style="background-color: #${note.color}">
-        <h3>${note.title}</h3>
+        <h3 class ="note-title">${note.title}</h3>
         <div class = del-vav-btn>
         <button class="favorite-btn">
             <img src="${note.isFavorite ? './images/icons/active.svg' : './images/icons/inactive.svg'}" alt="Изображение сердечка">
         </button>
-        <button class ="delite-btn"><img src="./images/icons/trash.svg" alt="Изображение сердечка"></button>
+        <button class ="delite-btn"><img src="./images/icons/trash.svg" alt="Изображение корзины"></button>
         </div>
     </div>
-    <p>${note.content}</p>
+    <p class="note-content">${note.content}</p>
 </li>
-            `;
+            `
             list.innerHTML = notesHTML;
-        }
-        
+        })
+
     },
+
+
+
+    renderNotesCount() {
+        const countNotes = document.querySelector('.count_notes');
+        countNotes.textContent = model.renderNotesCount();
+    },
+
+}
+
+
+
+const controller = {
+    renderNotesCount() {
+        viev.renderNotesCount()
+    },
+    addNote(title, content) {
+        if (title.trim() === '') return alert('Введите текст название')
+        if (title.trim().length >= 50) return alert('длина больше 50 символов')
+        if (content.trim() === '') return alert('Введите текст новой заметки')
+        model.addNote(title, content)
+    },
+    addColor(color) {
+        model.addColor(color)
+    },
+    addFavorite(noteId) {
+        model.addFavorite(noteId)
+    }
 }
 
 function init() {
@@ -89,21 +168,4 @@ function init() {
 
 init()
 
-const controller = {
-    renderNotesCount(){
-        view.renderNotesCount()
-    },
-
-
-}
-
-
-
-const colors = {
-    YELLOW: 'F3DB7D',
-    GREEN: 'C2F37D',
-    BLUE: '7DE1F3',
-    RED: 'F37D7D',
-    PURPLE: 'E77DF3',
-}
 
